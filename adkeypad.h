b9793dca4c73b15e7bf14674efaea450475c7d99
Sven Hesse <drmccoy@drmccoy.de>
2013-10-10 19:22:05 +0200

    ADKEYPAD: Stub AD Keypad implementation

---
+++ adkeypad.h
@@ -0,0 +1,43 @@
+/* adkeypad - Reading the keys Waveshare AD Keypad
+ *
+ * Copyright (c) 2013, Sven Hesse <drmccoy@drmccoy.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef ADKEYPAD_H_
+#define ADKEYPAD_H_
+
+#include "types.h"
+
+/** Read the state of the AD keypad.
+ *
+ *  Please see the notes in adc.h.
+ *
+ *  @param pin The pin (0-7) of port F the keypad is connected to.
+ *
+ *  @return The currently pressed key (0-15) or 0xFF if none is pressed.
+ */
+uint8_t adkeypad_get(uint8_t pin);
+uint16_t adkeypad_get_raw(uint8_t pin);
+
+#endif /* ADKEYPAD_H_ */
