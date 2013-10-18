b69af991ee0199a2c405d2aea2c334cf195b11db
Sven Hesse <drmccoy@drmccoy.de>
2013-10-18 11:11:34 +0200

    JANITORIAL: Align end of comment */ with /*

--- generic_io.h
+++ generic_io.h
@@ -22,7 +22,7 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ */
 
 /** @file generic_io.h
  *  These functions provide a method to abstract pin input/output. The hardware can then
