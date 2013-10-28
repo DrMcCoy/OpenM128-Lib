c251a18d8c945f84ae7d911cc0285a16f108fc0c
Sven Hesse <drmccoy@drmccoy.de>
2013-10-28 13:36:57 +0100

    AT24CXX: Implement read from/write to AT24CXX EEPROMs

---
+++ openm128/at24cxx.c
@@ -0,0 +1,158 @@
+/* at24cxx - Reading from / writing to the AT24CXX EEPROM
+ *
+ * Copyright (c) 2013, Sven Hesse <drmccoy@drmccoy.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "openm128/at24cxx.h"
+
+#define AT24CXX_BASE_ADDRESS (0xA0 >> 1)
+
+#define AT24CXX_SLAVE_ADDRESS(type, id, daddr) (AT24CXX_BASE_ADDRESS | ((daddr) >> 8) | ((id) << (type)))
+
+void at24cxx_init() {
+	i2c_init();
+}
+
+bool at24cxx_is_address_valid(at24cxx_type_t at24cxx, uint8_t device_id, uint16_t address) {
+	if (at24cxx >= kAT24CMAX)
+		// Not a valid type
+		return FALSE;
+
+	// Depending on the type, 8, 4, 2 or 1 device can be on one bus
+	if (device_id >= (1 << (3 - at24cxx)))
+		// Not a valid device ID for the type
+		return FALSE;
+
+	if (address >= at24cxx_size(at24cxx))
+		// Not a valid address for the type
+		return FALSE;
+
+	return TRUE;
+}
+
+uint16_t at24cxx_size(at24cxx_type_t at24cxx) {
+	// Depending on the type, 256, 512, 1024 or 2048 bytes of memory are available
+	return (1 << (8 + at24cxx));
+}
+
+bool at24cxx_poll(at24cxx_type_t at24cxx, uint8_t device_id, i2c_poll_t type) {
+	if (!at24cxx_is_address_valid(at24cxx, device_id, 0))
+		return FALSE;
+
+	return i2c_poll(AT24CXX_SLAVE_ADDRESS(at24cxx, device_id, 0), type);
+}
+
+bool at24cxx_poll_wait(at24cxx_type_t at24cxx, uint8_t device_id, bool write) {
+	if (!at24cxx_is_address_valid(at24cxx, device_id, 0))
+		return FALSE;
+
+	while (!at24cxx_poll(at24cxx, device_id, write));
+	return TRUE;
+}
+
+bool at24cxx_read_byte(at24cxx_type_t at24cxx, uint8_t device_id, uint16_t address, uint8_t *data) {
+	if (!at24cxx_is_address_valid(at24cxx, device_id, address))
+		return FALSE;
+
+	return i2c_read(AT24CXX_SLAVE_ADDRESS(at24cxx, device_id, address), address & 0x00FF, 1, data);
+}
+
+bool at24cxx_write_byte(at24cxx_type_t at24cxx, uint8_t device_id, uint16_t address, uint8_t data) {
+	if (!at24cxx_is_address_valid(at24cxx, device_id, address))
+		return FALSE;
+
+	return i2c_write(AT24CXX_SLAVE_ADDRESS(at24cxx, device_id, address), address & 0x00FF, 1, &data);
+}
+
+uint16_t at24cxx_read(at24cxx_type_t at24cxx, uint8_t device_id, uint16_t address, uint16_t n, uint8_t *data) {
+	if (!at24cxx_is_address_valid(at24cxx, device_id, address))
+		return 0;
+
+	uint16_t processed = 0;
+	while (n-- > 0) {
+		if (!at24cxx_poll_wait(kAT24C04, address, FALSE))
+			break;
+		if (!at24cxx_read_byte(at24cxx, device_id, address++, data++))
+			break;
+
+		processed++;
+	}
+
+	return processed;
+}
+
+uint16_t at24cxx_write(at24cxx_type_t at24cxx, uint8_t device_id, uint16_t address, uint16_t n, uint8_t *data) {
+	if (!at24cxx_is_address_valid(at24cxx, device_id, address))
+		return 0;
+
+	uint16_t processed = 0;
+	while (n-- > 0) {
+		if (!at24cxx_poll_wait(kAT24C04, address, TRUE))
+			break;
+		if (!at24cxx_write_byte(at24cxx, device_id, address++, *data++))
+			break;
+
+		processed++;
+	}
+
+	return processed;
+}
+
+bool at24cxx_read_word(at24cxx_type_t at24cxx, uint8_t device_id, uint16_t address, uint16_t *data) {
+	uint8_t tmp[2];
+	if (at24cxx_read(at24cxx, device_id, address, 2, tmp) != 2)
+		return FALSE;
+
+	*data = ((uint16_t)tmp[0] << 8) | tmp[1];
+	return TRUE;
+}
+
+bool at24cxx_write_word(at24cxx_type_t at24cxx, uint8_t device_id, uint16_t address, uint16_t data) {
+	uint8_t tmp[2];
+
+	tmp[0] = (data >> 8) & 0xFF;
+	tmp[1] =  data       & 0xFF;
+
+	return at24cxx_write(at24cxx, device_id, address, 2, tmp) == 2;
+}
+
+bool at24cxx_read_dword(at24cxx_type_t at24cxx, uint8_t device_id, uint16_t address, uint32_t *data) {
+	uint8_t tmp[4];
+	if (at24cxx_read(at24cxx, device_id, address, 4, tmp) != 4)
+		return FALSE;
+
+	*data = ((uint32_t)tmp[0] << 24) | ((uint32_t)tmp[1] << 16) | ((uint32_t)tmp[2] << 8) | tmp[3];
+	return TRUE;
+}
+
+bool at24cxx_write_dword(at24cxx_type_t at24cxx, uint8_t device_id, uint16_t address, uint32_t data) {
+	uint8_t tmp[4];
+
+	tmp[0] = (data >> 24) & 0xFF;
+	tmp[1] = (data >> 16) & 0xFF;
+	tmp[2] = (data >>  8) & 0xFF;
+	tmp[3] =  data        & 0xFF;
+
+	return at24cxx_write(at24cxx, device_id, address, 4, tmp) == 4;
+}
---
+++ openm128/at24cxx.h
@@ -0,0 +1,129 @@
+/* at24cxx - Reading from / writing to the AT24CXX EEPROM
+ *
+ * Copyright (c) 2013, Sven Hesse <drmccoy@drmccoy.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef AT24CXX_H_
+#define AT24CXX_H_
+
+#include "openm128/types.h"
+#include "openm128/i2c.h"
+
+/** The type of AT24CXX device / size of the memory. */
+typedef enum {
+	kAT24C02 = 0, ///< AT24C02:  2 kilobit ( 256 byte) of memory. 8 devices can be on one bus.
+	kAT24C04 = 1, ///< AT24C04:  4 kilobit ( 512 byte) of memory. 4 devices can be on one bus.
+	kAT24C08 = 2, ///< AT24C08:  8 kilobit (1024 byte) of memory. 2 devices can be on one bus.
+	kAT24C16 = 3, ///< AT24C16: 16 kilobit (2048 byte) of memory. 1 device  can be on one bus.
+
+	kAT24CMAX     ///< No real type; for range checks.
+} at24cxx_type_t;
+
+
+/** Initialize the I²C for accessing AT24CXX devices of the specified type. */
+void at24cxx_init();
+
+/** Is this combination of type, device ID and address valid? */
+bool at24cxx_is_address_valid(at24cxx_type_t at24cxx, uint8_t device_id, uint16_t address);
+
+/** Returns the size of the memory in bytes on this device type. */
+uint16_t at24cxx_size(at24cxx_type_t at24cxx);
+
+/** Query whether a specific AT24CXX device is ready to perform writing/reading.
+ *
+ *  @param at24cxx   The type of the AT24CXX device to poll.
+ *  @param device_id The ID of the device to poll.
+ *  @param type      The type of the operation to poll for.
+ *
+ *  @return TRUE if the device is ready to perform the specified operation.
+ */
+bool at24cxx_poll(at24cxx_type_t at24cxx, uint8_t device_id, i2c_poll_t type);
+
+/** Wait until a specific AT24CXX device is ready to perform writing/reading.
+ *
+ *  See at24cxx_poll().
+ *
+ *  @return TRUE if the device is ready to perform the specified operation, FALSE on invalid device.
+ */
+bool at24cxx_poll_wait(at24cxx_type_t at24cxx, uint8_t device_id, bool write);
+
+/** Read several bytes from an AT24CXX device.
+ *
+ *  @param at24cxx   The type of the AT24CXX device to read from.
+ *  @param device_id The ID of the device to read from.
+ *  @param address   The address from where to start reading.
+ *  @param n         The number of bytes to read.
+ *  @param data      The read data will be stored here.
+ *
+ *  @return The number of bytes successfully read.
+ */
+uint16_t at24cxx_read(at24cxx_type_t at24cxx, uint8_t device_id, uint16_t address, uint16_t n, uint8_t *data);
+
+/** Write several bytes to an AT24CXX device.
+ *
+ *  @param at24cxx   The type of the AT24CXX device to write to.
+ *  @param device_id The ID of the device to start writing to.
+ *  @param address   The address to write to.
+ *  @param n         The number of bytes to write.
+ *  @param data      The data to write.
+ *
+ *  @return The number of bytes successfully written.
+ */
+uint16_t at24cxx_write(at24cxx_type_t at24cxx, uint8_t device_id, uint16_t address, uint16_t n, uint8_t *data);
+
+/** Read a byte from an AT24CXX device.
+ *
+ *  @param at24cxx   The type of the AT24CXX device to read from.
+ *  @param device_id The ID of the device to read from.
+ *  @param address   The address from where to read.
+ *  @param data      The read data will be stored here.
+ *
+ *  @return TRUE if the reading was successful, FALSE otherwise.
+ */
+bool at24cxx_read_byte(at24cxx_type_t at24cxx, uint8_t device_id, uint16_t address, uint8_t *data);
+
+/** Write a byte to an AT24CXX device.
+ *
+ *  @param at24cxx   The type of the AT24CXX device to write to.
+ *  @param device_id The ID of the device to write to.
+ *  @param address   The address to write to.
+ *  @param data      The data to write.
+ *
+ *  @return TRUE if the writing was successful, FALSE otherwise.
+ */
+bool at24cxx_write_byte(at24cxx_type_t at24cxx, uint8_t device_id, uint16_t address, uint8_t data);
+
+/** Read a word (16 bit) from an AT24CXX device, MSB first. */
+bool at24cxx_read_word(at24cxx_type_t at24cxx, uint8_t device_id, uint16_t address, uint16_t *data);
+
+/** Write a word (16 bit) to an AT24CXX device, MSB first. */
+bool at24cxx_write_word(at24cxx_type_t at24cxx, uint8_t device_id, uint16_t address, uint16_t data);
+
+/** Read a double-word (32 bit) from an AT24CXX device, MSB first. */
+bool at24cxx_read_dword(at24cxx_type_t at24cxx, uint8_t device_id, uint16_t address, uint32_t *data);
+
+/** Write a double-word (32 bit) to an AT24CXX device, MSB first. */
+bool at24cxx_write_dword(at24cxx_type_t at24cxx, uint8_t device_id, uint16_t address, uint32_t data);
+
+#endif /* AT24CXX_H_ */
